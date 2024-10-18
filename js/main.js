2ba932dedacb58e6ca1e1f881760a1150f6a6e7e
João Montanholi <joao.montanholi@vml.com>
2024-10-18 15:23:21 +0100

    Change color palette and finish skeleton

--- js/main.js
+++ js/main.js
@@ -5,25 +5,46 @@ const randomGifPlaceholder = document.querySelector(
 );
 
 const generateGifMarkup = function (data) {
-  if (!data) return ``;
+  if (!data) return "";
 
   return `
-        <video class="gifVideo" width="320" height="240" autoplay loop muted>
-            <source src="${data.images.looping.mp4}" type="video/mp4">
-            <p>
-                ${
-                  data.alt_text
-                    ? data.alt_text
-                    : "Your browser does not support video"
-                }
-            </p>
-        </video>
-    `;
+    <video class="gifVideo" width="320" height="240" autoplay loop muted>
+        <source src="${data.images.looping.mp4}" type="video/mp4">
+        <p>
+            ${
+              data.alt_text
+                ? data.alt_text
+                : "Your browser does not support video"
+            }
+        </p>
+    </video>
+  `;
 };
 
-const fetchRandom = async function () {
+const generateLoadingSpinner = function () {
+  return `
+    <div class="loading">
+        <div class="spinner">
+        <span class="spinner__bar spinner__bar--1"></span>
+        <span class="spinner__bar spinner__bar--2"></span>
+        <span class="spinner__bar spinner__bar--3"></span>
+        <span class="spinner__bar spinner__bar--4"></span>
+        <span class="spinner__bar spinner__bar--5"></span>
+        <span class="spinner__bar spinner__bar--6"></span>
+        <span class="spinner__bar spinner__bar--7"></span>
+        <span class="spinner__bar spinner__bar--8"></span>
+        </div>
+        <p>Giphy's will be here in a jiffy...</p>
+    </div>
+  `;
+};
+
+const renderRandom = async function () {
+  const skeleton = randomGifPlaceholder.querySelector(".gifSkeleton");
+
   try {
-    // TODO: add loading message
+    skeleton.insertAdjacentHTML("beforeend", generateLoadingSpinner());
+
     const response = await fetch(
       `${BASE_API_URL}/random?api_key=${API_KEY}231`
     );
@@ -35,19 +56,18 @@ const fetchRandom = async function () {
 
     const { data } = await response.json();
 
-    const skeleton = randomGifPlaceholder.querySelector(".gifSkeleton");
-    console.log(skeleton);
-
     // randomGifPlaceholder.insertAdjacentHTML(
     //   "beforeend",
     //   generateGifMarkup(data)
     // );
   } catch (error) {
     // TODO: Show error message instead of loading
+    console.log(error);
+
     console.log("CATCH", JSON.parse(error.message));
   } finally {
-    // TODO: remove loading
+    skeleton.querySelector(".loading").remove();
   }
 };
 
-fetchRandom();
+renderRandom();
