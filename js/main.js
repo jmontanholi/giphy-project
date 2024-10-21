0d9ac815de1b84b62482b2f5fed98b1e9a10e563
João Montanholi <joao.montanholi@vml.com>
2024-10-21 08:34:50 +0100

    Finish random section

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ const randomGifPlaceholder = document.querySelector(
 );
 const randomNextBtn = document.querySelector(".randomSection__btn");
 
-const generateGifMarkup = function (data) {
+/**
+ *
+ * @param {object} parent // Node element that should have its content replaced by skeleton
+ *
+ * This function appends the gif to the parent at the end
+ */
+const generateGifMarkup = function (data, parent) {
   if (!data) return "";
 
-  return `
+  const markup = `
     <video class="gifVideo" width="280" autoplay loop muted>
         <source src="${data.images.looping.mp4}" type="video/mp4">
         <p>
@@ -20,10 +26,18 @@ const generateGifMarkup = function (data) {
         </p>
     </video>
   `;
+
+  parent.insertAdjacentHTML("beforeend", markup);
 };
 
-const generateLoadingSpinner = function () {
-  return `
+/**
+ *
+ * @param {object} parent // Node element that should have its content replaced by skeleton
+ *
+ * This function appends the loading message to the parent at the end
+ */
+const generateLoadingMessage = function (parent) {
+  const markup = `
     <div class="loading">
         <div class="spinner">
         <span class="spinner__bar spinner__bar--1"></span>
@@ -38,41 +52,78 @@ const generateLoadingSpinner = function () {
         <p>Giphy's will be here in a jiffy...</p>
     </div>
   `;
+
+  parent.insertAdjacentHTML("beforeend", markup);
 };
 
-const generateErrorMessage = function () {
-  return `
+/**
+ *
+ * @param {object} parent // Node element that should have its content replaced by skeleton
+ *
+ * This function appends the error message to the parent at the end
+ */
+const generateErrorMessage = function (parent) {
+  const markup = `
     <div class="error">
         <i class="fa-solid fa-triangle-exclamation"></i>
         <p>Oh no, something went snap!</p>
     </div>
   `;
+
+  parent.insertAdjacentHTML("beforeend", markup);
+};
+
+/**
+ *
+ * @param {object} parent // Node element that should have its content replaced by skeleton
+ *
+ * This function replaces parent content with Skeleton
+ */
+const renderSkeleton = function (parent) {
+  const markup = `
+    <div class="gifSkeleton" aria-hidden="true">
+      <i class="fa-regular fa-image skeleton__svg"> </i>
+    </div>
+  `;
+
+  parent.innerHTML = markup;
 };
 
 const renderRandom = async function () {
+  // First add to the parent or replace the current gif with a skeleton
+  renderSkeleton(randomGifPlaceholder);
+
+  // Select the skeleton to use later
   const skeleton = randomGifPlaceholder.querySelector(".gifSkeleton");
 
   try {
-    skeleton.insertAdjacentHTML("beforeend", generateLoadingSpinner());
+    // Add the loading message with the spinner
+    generateLoadingMessage(skeleton);
 
-    const response = await fetch(`${BASE_API_URL}/random?api_key=${API_KEY}`);
+    // Call the API to request random gif
+    const response = await fetch(`${BASE_API_URL}/random?api_key=${API_KEY}2`);
 
+    // Return API error if something goes wrong
     if (!response.ok) {
       const { meta: errorData } = await response.json();
       throw Error(JSON.stringify(errorData));
     }
 
+    // Destructure data from the response
     const { data } = await response.json();
 
+    // Remove the skeleton and render gif
     skeleton.remove();
-    randomGifPlaceholder.insertAdjacentHTML(
-      "beforeend",
-      generateGifMarkup(data)
-    );
+    generateGifMarkup(data, randomGifPlaceholder);
   } catch (error) {
-    skeleton.insertAdjacentHTML("beforeend", generateErrorMessage());
-  } finally {
+    // If something goes wrong we remove the loading and append the error message
     skeleton.querySelector(".loading").remove();
+    generateErrorMessage(skeleton);
   }
 };
-renderRandom();
+
+// Add event listener to next button on random section to render a random gif
+randomNextBtn.addEventListener("click", renderRandom);
+
+// Call render random a first time when page is loaded
+// renderRandom();
